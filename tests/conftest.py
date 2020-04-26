0de7ff44e3fc3317e483be517313d93457132743
Ilya Zhiltsov <izhiltsov@apple.com>
2020-04-26 15:59:51 -0700

    Initial commit

---
+++ tests/conftest.py
@@ -0,0 +1,30 @@
+import json
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture
+def config():
+	# Read the file
+	with open('config.json') as f:
+		config = json.load(f)
+
+	# Assert values are acceptable
+	assert config['browser'] in ['firefox', 'safari', 'chrome']
+	assert isinstance(config['implicit_wait'], int)
+	assert config['implicit_wait'] > 0
+
+	# Return config so it can be used
+	return config
+
+
+
+
+#@pytest.fixture(params=[webdriver.Chrome, webdriver.Safari, webdriver.Firefox], ids=['Chrome', 'Safari', 'Firefox'])
+@pytest.fixture(params=[webdriver.Chrome], ids=['Chrome'])
+def browser(request):
+	driver = request.param
+	driver = driver()
+	driver.implicitly_wait(10)
+	yield driver
+	driver.quit()
---
+++ tests/test_main_page.py
@@ -0,0 +1,8 @@
+from selenium.webdriver.common.by import By
+
+
+def test_guest_can_go_to_login_page(browser):
+    url = "http://selenium1py.pythonanywhere.com/"
+    browser.get(url)
+    login_element = browser.find_element(By.CSS_SELECTOR, '#login_link')
+    login_element.click()
